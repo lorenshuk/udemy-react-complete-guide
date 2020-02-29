5f69dad4ecbcecd665bf8d42e9bad4cdec78cc45
loren inada <lordawg375@sbcglobal.net>
2020-02-28 20:18:21 -0800

    Done. 2/28/20 8:18 PM

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-/*  Replaced for Lesson 28 2/25/20 7:10 PM
+     /*  Replaced for Lesson 28 2/25/20 7:10 PM
      <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -15,10 +15,9 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
- */    
-      <div className="App">
-        <h1>Hi, I'm a React App!</h1>
-      </div>
+      */    
+     // Lesson 30: Understanding JSX
+      React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React Element'))
    );
   }
 }
