ef09df29c3d08b3eb1aa70915c452308608cba55
loren inada <lordawg375@sbcglobal.net>
2020-03-07 20:28:28 -0800

    Done. 3/7/20 8:28 PM

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
-     /*  Replaced for Lesson 28 2/25/20 7:10 PM
-     <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App">
+        <h1>Hi, I’m a React App</h1>
+        <p>This is really working!</p>
+        <Person />
       </div>
-      */    
-     // Lesson 30: Understanding JSX
-      React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React Element'))
-   );
+    );
   }
 }
-
+     
 export default App;
