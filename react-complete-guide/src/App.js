b6ccf11e1fea37cd6b70c596bcfe398184ceb713
loren inada <lordawg375@sbcglobal.net>
2020-03-11 19:20:38 -0700

    Done. 3/11/20 7:20 PM

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,11 +3,27 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28},
+      { name: 'Manu', age: 29},
+      { name: 'Stephanie', age: 26}
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('SwitchNameHandler was clicked')
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I’m a React App</h1>
         <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
         <Person name="Holdstein" age="38">My Hobbies: Racing</Person>
       </div>
     );
