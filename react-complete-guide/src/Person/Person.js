e63a8610ab4f947164f54159192bfab38f2f1109
loren inada <lordawg375@sbcglobal.net>
2020-04-06 21:15:09 -0700

    done 4/6/20 9:15 PM

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.css';
 // import Radium from 'radium';
 import styled from 'styled-components'
 
@@ -17,13 +17,11 @@ const StyledDiv = styled.div`
 `
 const person = (props) => {
     return (
-       // <div className="Person" style={style}>
-        <StyledDiv>
+       <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
-        </StyledDiv>
-        // </div>
+       </div>
     ) 
 };
 
