9ba2ae5f17caa3a48cc13f7ab13abe027d8cd4c5
loren inada <lordawg375@sbcglobal.net>
2020-04-10 17:18:22 -0700

    Closeout this branch 4/10/20

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,22 +1,5 @@
 import React from 'react';
 import classes from './Person.css';
-// import Radium from 'radium';
-import styled from 'styled-components'
-
-/*
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2-x 3-x #ccc;
-    padding: 16px;
-    text-align: center;
-
-    @media (min-width: 500px) {
-        width: 450px;
-    }
-`
-*/
 
 const person = (props) => {
     return (
