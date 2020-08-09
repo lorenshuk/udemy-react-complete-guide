7d18e3162c3336feedaccea67eb7df273d654081
loren inada <lordawg375@sbcglobal.net>
2020-08-08 21:35:08 -0700

    Done L125. 8/8/20 9:35 PM

--- burgerbuilder/src/components/Layout/Layout.js
+++ burgerbuilder/src/components/Layout/Layout.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import Aux from '../../hoc/Aux'
-import classes from './Layout.css'
+import React from 'react';
+import Aux from "../../hocs/Auxiliary";
+import classes from './Layout.css';
 
 const layout = (props) => (
     <Aux>
