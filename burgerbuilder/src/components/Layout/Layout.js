03a85cb6bcc915eb5953819d296eef909aad3bf2
loren inada <lordawg375@sbcglobal.net>
2020-08-09 21:44:59 -0700

    Done. 8/9/20 9:44 PM

--- burgerbuilder/src/components/Layout/Layout.js
+++ burgerbuilder/src/components/Layout/Layout.js
@@ -6,7 +6,7 @@ const layout = (props) => (
     <Aux>
         <div>Toolbar, Sidedrawer, Backdrop Up Here</div>
 
-        <main className={classes.content}>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
