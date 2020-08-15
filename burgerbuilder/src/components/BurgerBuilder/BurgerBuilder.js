7b6f487cd4160cf1444b8e9a9cdbdffe020326bd
loren inada <lordawg375@sbcglobal.net>
2020-08-14 19:50:13 -0700

    Done. 8/14/20 7:50 PM

--- burgerbuilder/src/components/BurgerBuilder/BurgerBuilder.js
+++ burgerbuilder/src/components/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,14 @@
 import React, { Component } from 'react';
 import Aux from '../../hocs/Auxiliary';
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
 
 class BurgerBuilder extends Component {
     /* 8/13/20 Add state for the dynamic Ingredient build ~ Lesson 129 */
@@ -14,17 +22,55 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
-        }
+        },
+        totalPrice: 0
+    }
+
+    addIngredientHandler = (type) => {
+        const updatedCount = this.state.ingredients[type] + 1;
+        let updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const newPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    }
+
+    removeIngredientHandler = (type) => {
+        const updatedCount = this.state.ingredients[type] - 1;
+        
+        if (updatedCount < 0)
+            return
+
+        let updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const newPrice = this.state.totalPrice - INGREDIENT_PRICES[type];
+        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
     }
 
     render() {
-            return (
+        const disabledInfo = {
+            ...this.state.ingredients
+        }
+
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
+
+        return (
             <Aux>
                 {/* Visual representation of the Burger 
                     <div>Burger</div> -> Replaced with the <Burger> Class-Component - 8/13/20
                 */}
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
+                <BuildControls 
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo}
+                    price={this.state.totalPrice}
+                />
             </Aux>
         )
     }
---
+++ burgerbuilder/test.js
@@ -0,0 +1,12 @@
+const testX = {
+    test1: 'one',
+    test2: 'two',
+    test3: 'three',
+    test4: 'four'
+};
+const testY = {
+    test10: 'ten',
+    test11: 'eleven',
+};
+
+console.log({...testX, ...testY})
