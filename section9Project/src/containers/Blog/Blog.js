cc2a6fdc26c9dc898ed12ffccb3663fa1716a10d
loren inada <lordawg375@sbcglobal.net>
2021-04-05 20:38:07 -0700

    Done Lesson 165. 4/5/21 8:38 PM

--- section9Project/src/containers/Blog/Blog.js
+++ section9Project/src/containers/Blog/Blog.js
@@ -5,23 +5,52 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+//import post from '../../components/Post/Post';
 
 class Blog extends Component {
+    state = {
+        posts: [],
+        selectedPostId: 0
+    }
+
+    /* Fetch the blog dummy data with Axios
+        - Fetch after the component has been rendered "componentDIDMount" */
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/todos')
-    		.then(response => console.log(response.data))
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+    		.then(response => {
+                const posts = response.data.slice(0, 4)
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                })
+                this.setState({posts: updatedPosts})
+            })
     }
     
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id})
+    }
+
+
     render () {
+        const posts = this.state.posts.map( post => {
+            return <Post 
+                        key={post.id} 
+                        title={post.title} 
+                        author={post.author} 
+                        clicked={() => this.postSelectedHandler(post.id) }
+                    />
+        })
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
