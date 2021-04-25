8084132d6b95ead97c527ba7bbf499dc8e564314
loren inada <lordawg375@sbcglobal.net>
2021-04-24 19:52:42 -0700

    Done Lesson 167 4/24/21 7:52 PM

--- section9Project/src/components/NewPost/NewPost.js
+++ section9Project/src/components/NewPost/NewPost.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import './NewPost.css';
 
 class NewPost extends Component {
     state = {
         title: '',
-        content: '',
+        body: '',
         author: 'Max'
+    }
+
+    postDataHandler = () => {
+        const postData = {
+            title: this.state.title,
+            body: this.state.body,
+            author: this.state.author
+        }
+
+        axios.post('https://jsonplaceholder.typicode.com/posts/', postData)
+            .then(response => {
+                console.log(response)
+            })
+
     }
 
     render () {
@@ -15,14 +30,14 @@ class NewPost extends Component {
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
-                <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+                <label>body</label>
+                <textarea rows="4" value={this.state.body} onChange={(event) => this.setState({body: event.target.value})} />
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
