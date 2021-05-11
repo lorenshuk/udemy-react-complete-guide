81ef5f9a453819c86cf91c41a49072ab38b8e6ab
loren inada <lordawg375@sbcglobal.net>
2021-05-10 18:14:13 -0700

    Done. Updated Course Lesson 29 5/10/21 6:14 PM

--- burgerbuilder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ burgerbuilder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import classes from './BuildControl.css';
+import React from "react";
+import classes from "./BuildControl.css";
 
 const buildControl = (props) => (
-    <div className={classes.BuildControl}>
-        <div className={classes.label}>{props.label}</div>
-        <button 
-            className={classes.Less} 
-            onClick={props.removed}
-            disabled={props.disabled}
-        >Less</button>
-        
-        <button 
-            className={classes.More} 
-            onClick={props.added}
-        >More</button>
-    </div>
+  <div className={classes.BuildControl}>
+    <div className={classes.label}>{props.label}</div>
+    <button
+      className={classes.Less}
+      onClick={props.removed}
+      disabled={props.disabled}
+    >
+      Less
+    </button>
+
+    <button className={classes.More} onClick={props.added}>
+      More
+    </button>
+  </div>
 );
 
-export default buildControl
+export default buildControl;
