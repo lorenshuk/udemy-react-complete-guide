e4eb907fac1ddbf541f235610ea48fdec5a1b089
loren inada <lordawg375@sbcglobal.net>
2021-07-21 21:30:13 -0700

    7/21/21 9:29 PM Lesson 36 done

--- 05 Updated Course May 2021/react-complete-guide/src/App.js
+++ 05 Updated Course May 2021/react-complete-guide/src/App.js
@@ -1,10 +1,21 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  // Sample data for testing 7/21/21
+  const expenses = [
+    { title: "Rent", amount: 1100.0, date: new Date(2021, 0, 1) },
+    { title: "Car Insurance", amount: 295.67, date: new Date(2021, 6, 21) },
+    { title: "Health Insurance", amount: 300.0, date: new Date(2021, 6, 28) },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- 05 Updated Course May 2021/react-complete-guide/src/components/ExpenseItem.js
+++ 05 Updated Course May 2021/react-complete-guide/src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const expenseDate = new Date(2021, 6, 21);
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">Amount</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
