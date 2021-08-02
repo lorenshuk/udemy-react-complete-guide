d03a5117fb0191d2cbbfa80fd38d9f98df626ddd
loren inada <lordawg375@sbcglobal.net>
2021-08-01 20:39:35 -0700

    Done Lesson 39 8/1/21 8:39 PM

--- 05 Updated Course May 2021/react-complete-guide/src/components/Expenses.js
+++ 05 Updated Course May 2021/react-complete-guide/src/components/Expenses.js
@@ -1,13 +1,14 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from './Card';
 import "./Expenses.css";
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {
         props.expenses.map(expense => (<ExpenseItem key={expense.id} expense={expense} />))
       }
-    </div>
+    </Card>
   );
 }
 
