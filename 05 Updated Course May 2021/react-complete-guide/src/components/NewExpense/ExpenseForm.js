7fce4428b82f40e0c236efbb3e7b60dcd77af4aa
loren inada <lordawg375@sbcglobal.net>
2021-08-14 20:54:46 -0700

    Done Lesson 56 8/14/21 8:54 PM

--- 05 Updated Course May 2021/react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ 05 Updated Course May 2021/react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -19,8 +19,22 @@ const ExpenseForm = () => {
     setEnteredDate(event.target.value);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    };
+
+    // Test the prograom
+    console.log(expenseData);
+  };
+  
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
